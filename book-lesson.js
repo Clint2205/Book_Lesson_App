a7b80aad63649ca2aaa466164d9d31adc2704a45
Your Name <swifty2205@yahoo.co.uk>
2023-10-05 09:24:46 +0100

    Implemented addcart logic

--- book-lesson.js
+++ book-lesson.js
@@ -3,30 +3,39 @@ const app = Vue.createApp({
     return {
       //array of lesson objects
       lessons: [
-        { subject: "Mathematics", location: "Manchester", price: 80, spaces: 5, icon: "fa-calculator" },
-        { subject: "English", location: "London", price: 100, spaces: 5, icon: "fa-book" },
-        { subject: "Drama", location: "London", price: 90, spaces: 5, icon: "fa-theater-masks" },
-        { subject: "Science", location: "Bristol", price: 70, spaces: 5, icon: "fa-flask" },
-        { subject: "English", location: "Birmingham", price: 120, spaces: 5, icon: "fa-book" },
-        { subject: "Music", location: "Nottingham", price: 110, spaces: 5, icon: "fa-music" },
-        { subject: "History", location: "Liverpool", price: 90, spaces: 5, icon: "fa-history" },
-        { subject: "Chess", location: "Leeds", price: 90, spaces: 5, icon: "fa-chess" },
-        { subject: "English", location: "Coventry", price: 100, spaces: 5, icon: "fa-book" },
-        { subject: "History", location: "Liverpool", price: 90, spaces: 5, icon: "fa-history" },
-        { subject: "Drama", location: "London", price: 90, spaces: 5, icon: "fa-theater-masks" },
-        { subject: "English", location: "London", price: 100, spaces: 5, icon: "fa-book" },
+        { subject: "Mathematics", location: "Manchester", price: 80, spaces: 5, cart: 0, icon: "fa-calculator" },
+        { subject: "English", location: "London", price: 100, spaces: 5, cart: 0, icon: "fa-book" },
+        { subject: "Drama", location: "London", price: 90, spaces: 5, cart: 0, icon: "fa-theater-masks" },
+        { subject: "Science", location: "Bristol", price: 70, spaces: 5, cart: 0, icon: "fa-flask" },
+        { subject: "English", location: "Birmingham", price: 120, spaces: 5, cart: 0, icon: "fa-book" },
+        { subject: "Music", location: "Nottingham", price: 110, spaces: 5, cart: 0, icon: "fa-music" },
+        { subject: "History", location: "Liverpool", price: 90, spaces: 5, cart: 0, icon: "fa-history" },
+        { subject: "Chess", location: "Leeds", price: 90, spaces: 5, cart: 0, icon: "fa-chess" },
+        { subject: "English", location: "Coventry", price: 100, spaces: 5, cart: 0, icon: "fa-book" },
+        { subject: "History", location: "Liverpool", price: 90, spaces: 5, cart: 0, icon: "fa-history" },
+        { subject: "Drama", location: "London", price: 90, spaces: 5, cart: 0, icon: "fa-theater-masks" },
+        { subject: "English", location: "London", price: 100, spaces: 5, cart: 0, icon: "fa-book" },
       ],
-      bookings: [],
       //store slections temporarily
       selectedSort: "",
       selectedSortOrder: '',
+      cartEmpty: true,
+      // Track the current view cart or lesson
+      lessonView: true,
+      cart: [],
+      toggleButtonText: "View Cart", 
     };
   },
   methods: {
     bookLesson(lesson) {
-      // Pushing the booked lesson to the bookings array
-      this.bookings.push(`Booked ${lesson.subject} for ${lesson.price}`);
-      console.log(this.bookings);
+      if (lesson.spaces > 0) {
+        // Decrease the spaces count by 1 and  Update the shopping cart or perform other actions as needed
+       this.cart.push(lesson);   
+        lesson.spaces--; 
+        this.cartEmpty = false;
+       
+        
+      }
     },
     sortLessons() {
       //  copy of lessons  array
@@ -34,7 +43,7 @@ const app = Vue.createApp({
 
       // Conditionally checking the selected sorting criteria 
       if (this.selectedSort === 'subject' || this.selectedSort === 'location') {
-        // Sort alphabetically by subject or location 
+        // Sorting alphabetically by subject or location 
         sorted.sort((a, b) => {
           const aValue = a[this.selectedSort].toLowerCase();
           const bValue = b[this.selectedSort].toLowerCase();
@@ -53,6 +62,24 @@ const app = Vue.createApp({
       // Populate  lessons array with the sorted results
       this.lessons = [...sorted];
     },
+    toggleCart() {
+      // Toggle between the lesson view andshopping cart
+     this.lessonView = !this.lessonView;
+     this.toggleButtonText = this.lessonView ? "View Cart" : "Back to Lessons";
+      console.log("eeeee")
+  },
+  // Increase the available space when cart item is removed from the cart
+  removeFromCart(index) {
+    const removedLesson = this.cart[index];
+    this.cart.splice(index, 1); 
+    removedLesson.spaces += 1; 
+    // Check if the cart is empty after removing an item
+  if (this.cart.length === 0) {
+    this.cartEmpty = true;
+  }
+    
+  },
+    
   },
   computed: {
     // Computed property to return the sorted lessons
